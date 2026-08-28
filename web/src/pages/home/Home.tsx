c068483b9782b01f1be0b3f0e636d7753a60f287
Frostyrr <marcelson.rebato@gmail.com>
2026-08-28 15:24:54 +0800

    fix: update import statements and reorder components in Home page

--- web/src/pages/home/Home.tsx
+++ web/src/pages/home/Home.tsx
@@ -1,23 +1,20 @@
-import React from 'react';
-import { Hero } from './sections/Hero';
-import { Features } from './sections/Features';
-import { HowItWorks } from './sections/HowItWorks';
-import { History } from './sections/History';
-import { CommunityCTA } from './sections/CommunityCTA';
+import React from "react";
+import { Hero } from "./sections/Hero";
+import { Features } from "./sections/Features";
+import { HowItWorks } from "./sections/HowItWorks";
+import { History } from "./sections/History";
+import { CommunityCTA } from "./sections/CommunityCTA";
 
 export const Home: React.FC = () => {
   return (
     <main className="flex-1 flex flex-col w-full">
       <Hero />
+      <History />
       <Features />
       <HowItWorks />
-      <History />
       <CommunityCTA />
     </main>
   );
 };
 
 export default Home;
-
-
-
